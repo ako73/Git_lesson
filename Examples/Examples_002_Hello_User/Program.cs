afe0fd44d2c63dd72435fe82dda12922d9919302
Konstantin <ako73@yandex.ru>
2022-08-19 19:24:40 +0500

    добавил комментарии

--- Examples/Examples_002_Hello_User/Program.cs
+++ Examples/Examples_002_Hello_User/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿Console.Write("Введите ваше имя: "); //Выводит значение в скобках
+string username = Console.ReadLine();//Считывает данные из консоли
+Console.Write("Привет, ");//Выводит значение в скобках
+Console.WriteLine(username);//Выводит значение в скобках и делает перевод строки

6b11105b31f6bce65cb8c4a7b648783d034b4bbe
Konstantin <ako73@yandex.ru>
2022-08-20 13:52:21 +0500

    .\Example_003_Sum

---
+++ Examples/Example_003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿//Console.Write("Введите число А: ");
+int numberA = 31; //Console.ReadLine();
+//Console.Write("Введите число В: ");
+int numberB = 55; //Console.ReadLine();
+int result = numberA+numberB;
+Console.WriteLine(result);

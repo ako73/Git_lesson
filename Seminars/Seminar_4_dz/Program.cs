125da47c74b73fe46abae68f216fbb6e932c714c
Konstantin <ako73@yandex.ru>
2022-09-05 22:14:40 +0500

    Добавлено ДЗ по семинару 4

---
+++ Seminars/Seminar_4_dz/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// int Exponentiation(int numberA, int numberB){
+//   int result = 1;
+//   for(int i=1; i <= numberB; i++){
+//     result = result * numberA;
+//   }
+//     return result;
+// }
+//     Console.Write("Введите число A: ");
+//   int numberA = Convert.ToInt32(Console.ReadLine());
+//   Console.Write("Введите число B: ");
+//   int numberB = Convert.ToInt32(Console.ReadLine());
+
+//   int exponentiation = Exponentiation(numberA, numberB);
+//   Console.WriteLine("Ответ: " + exponentiation);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+// Задача 29: Напишите программу, которая задаёт массив из введенного числа и выводит их на экран.
+// пользователь вводит число элементов в массиве и минимальное, и максимальное значение . надо заполнить массив рандоными данными от минимального до максимального значение .И важное уточнениее , делать все в методе который возвращает массив
+// 5, 1, 20 -> [1, 2, 5, 7, 19]
+// 3, 1, 33 -> [6, 1, 33]

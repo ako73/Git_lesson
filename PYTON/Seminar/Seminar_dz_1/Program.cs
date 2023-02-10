3b7a89e9ca5fc7d87450ebd4ae6f28c923fb98b7
Konstantin <ako73@yandex.ru>
2023-02-10 10:17:25 +0500

    Синхрон

--- PYTON/Seminar/Seminar_dz_1/Program.cs
+++ PYTON/Seminar/Seminar_dz_1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿# 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+# Пример:
+# 6 -> да
+# 7 -> да
+# 1 -> нет
+
+# day = int(input('Enter day number: '))
+# if day > 7 or day < 1:
+#     print('Please, repeat the input')
+# elif day == 6 or day == 7:
+#     print("Yes, it's weekend!")
+# else:
+#     print("No, it's not weekend!")
+
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+# Пример:
+
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+# x = int(input('Введите число x '))
+# y = int(input('Введите число y '))
+# z = int(input('Введите число z '))
+
+# a = x * y * z
+# b = x + y + z
+
+# if a > 0:
+#     a = 0
+# elif a < 1:
+#     a = 1
+# if b > 0:
+#     b = 1
+# elif b < 1:
+#     b = 1
+
+# if a == b:
+#     print('Утверждение истинно')
+# elif a != b:
+#     print('Утверждение ложно')
+
+# leftSide = not (x or y or z)
+# rightSide = not x and not y and not z
+# result = leftSide == rightSide
+
+# if result == True:
+#     print('Утверждение истинно')
+# else:
+#     print('Утверждение ложно')
